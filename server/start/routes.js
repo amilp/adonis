98ffb6474a3361c9614456fe83af1943077909fc
Amil P <amil@outlook.com>
2018-05-24 00:07:05 +0530

    login

--- server/start/routes.js
+++ server/start/routes.js
@@ -14,7 +14,10 @@
 */
 
 const Route = use('Route')
+Route
+  .group(() => {
+    Route.post('auth/register', 'UserController.register');
+    Route.post('auth/login', 'UserController.login')
+  })
+  .prefix('api')
 
-Route.get('/', ({ request }) => {
-  return { greeting: 'Hello world in JSON' }
-})
